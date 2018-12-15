24f1f515d5541b520e5300c7834b53b4592a4e26
10kaoru12 <10kaoru12@gmail.com>
2018-12-15 17:53:47 +0900

    内容の改修

--- index_script.js
+++ index_script.js
@@ -1,10 +1,3 @@
 $(function() {
     $('.drawer').drawer();
 });
-$(document).ready(function(){
-    $("#fakeloader").fakeLoader({
-        timeToHide: 1200,
-        spinner: "spinner3",
-        bgColor: "skyblue"
-    });
-});
