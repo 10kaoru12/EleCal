bfd3a934bd769d856729e294888739a89715aa83
10kaoru12 <10kaoru12@gmail.com>
2018-12-15 11:18:59 +0900

    fakelodingを追加 内容の改修

--- index_script.js
+++ index_script.js
@@ -1,3 +1,10 @@
 $(function() {
     $('.drawer').drawer();
 });
+$(document).ready(function(){
+    $("#fakeloader").fakeLoader({
+        timeToHide: 1200,
+        spinner: "spinner3",
+        bgColor: "skyblue"
+    });
+});
